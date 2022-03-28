f4a5d4caefabf7d3ca55911bc1d91404dd2f3083
lyfe00011 <lyfe00011@gmail.com>
2022-03-28 13:17:09 +0530

    fix

--- plugins/profile.js
+++ plugins/profile.js
@@ -5,7 +5,7 @@ bot(
 	{ pattern: 'jid', fromMe: fm, desc: '', type: 'misc' },
 	async (message, match) => {
 		return await message.sendMessage(
-			message.mention[0] || message.reply_message.jif || message.jid
+			message.mention[0] || message.reply_message.jid || message.jid
 		)
 	}
 )
