8b0123b539ff653135665a807bdf44e7820bd076
lyfe00011 <lyfe00011@gmail.com>
2022-03-31 22:00:41 +0530

    updated

--- plugins/profile.js
+++ plugins/profile.js
@@ -9,3 +9,29 @@ bot(
 		)
 	}
 )
+
+bot(
+	{ pattern: 'block', fromMe: fm, desc: '', type: 'misc' },
+	async (message, match) => {
+		const id =
+			message.mention[0] ||
+			message.reply_message.jid ||
+			(!message.isGroup && message.jid)
+		if (!id) return await message.sendMessage('*Give me a person*')
+		await message.sendMessage('_Blocked_')
+		await message.Block(id)
+	}
+)
+
+bot(
+	{ pattern: 'unblock', fromMe: fm, desc: '', type: 'misc' },
+	async (message, match) => {
+		const id =
+			message.mention[0] ||
+			message.reply_message.jid ||
+			(!message.isGroup && message.jid)
+		if (!id) return await message.sendMessage('*Give me a person*')
+		await message.sendMessage('_Unblocked_')
+		await message.Unblock(id)
+	}
+)
